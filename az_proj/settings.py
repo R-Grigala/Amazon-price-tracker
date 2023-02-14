4088e6bf67b60ad78313af90f2f5a59a74a85257
R-Grigala <r.grigalashvili777@gmail.com>
2023-02-14 17:04:04 +0400

    Coding Continue:// change little details

--- az_proj/settings.py
+++ az_proj/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$xlq)r@l-d)w$*+2dk6y_ltm++99^+^=(dy1kng9uzke3y9ov0"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',  default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
